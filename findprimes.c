7ded5701af5e14bc389b40eecea62fce1cc92167
Stefan Teleman <stefan.teleman@gmail.com>
2023-05-21 09:56:49 -0400

    Added OpenMP.

--- findprimes.c
+++ findprimes.c
@@ -24,11 +24,8 @@
 #include <math.h>
 #include <time.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <errno.h>
-
-#if defined(_OPENMP)
-#include <omp.h>
-#endif
 
 static uint64_t range_start = 1UL;
 static uint64_t range_end = 0UL;
@@ -37,25 +34,11 @@ static uint64_t prime_index = 0UL;
 static uint64_t* prime_storage = NULL;
 static bool print_header = false;
 static bool print_timestamp = false;
-static bool print_mersenne = false;
 static struct timespec ts_begin = { 0, 0 };
 static struct timespec ts_end = { 0, 0 };
 static struct timespec ts_correct = { 0, 0 };
 
-static const uint64_t PT[] = {
-  1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192,
-  16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152,
-  4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456,
-  536870912, 1073741824, 2147483648, 4294967296, 8589934592, 17179869184,
-  34359738368, 68719476736, 137438953472, 274877906944, 549755813888,
-  1099511627776, 2199023255552, 4398046511104, 8796093022208, 17592186044416,
-  35184372088832, 70368744177664, 140737488355328, 281474976710656,
-  562949953421312, 1125899906842624, 2251799813685248, 4503599627370496,
-  9007199254740992, 18014398509481984, 36028797018963968, 72057594037927936,
-  144115188075855872, 288230376151711744, 576460752303423488,
-  1152921504606846976, 2305843009213693952, 4611686018427387904,
-  9223372036854775808
-};
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static void print_help(void)
 {
@@ -63,7 +46,6 @@ static void print_help(void)
   (void) fprintf(stderr, "       [ -e <range-end> "
                          "(default ULONG_MAX | ULLONG_MAX)]\n");
   (void) fprintf(stderr, "       [ -b <number-of-bits> (default 64)]\n");
-  (void) fprintf(stderr, "       [ -M <identify Mersenne primes>]\n");
   (void) fprintf(stderr, "       [ -f <output-file> (default stdout)]\n");
   (void) fprintf(stderr, "       [ -p <print header at the top>]\n");
   (void) fprintf(stderr, "       [ -t <print prime discovery time>]\n");
@@ -79,7 +61,7 @@ static int qsort_compare(const void* l, const void* r)
 static void timestamp(struct timespec* ts)
 {
   errno = 0;
-  if (clock_gettime(CLOCK_PROCESS_CPUTIME_ID, ts) != 0) {
+  if (clock_gettime(CLOCK_MONOTONIC, ts) != 0) {
     (void) fprintf(stderr, "Could not get the timestamp: %s\n",
                    strerror(errno));
   }
@@ -105,23 +87,17 @@ static void print_time(const char* filename)
   ts_diff.tv_sec -= ts_correct.tv_sec;
   ts_diff.tv_nsec -= ts_correct.tv_nsec;
 
-  long double t = (double) ts_diff.tv_nsec / 1000000000;
-  t += (long double) ts_diff.tv_sec;
-
-#if defined(_OPENMP)
-  t /= omp_get_max_threads();
-#endif
-
   (void) fprintf(fp, "-----\n");
-  (void) fprintf(fp, "Discovered %lu prime numbers in %Lf seconds.\n",
-                 prime_index, t);
+  (void) fprintf(fp, "Discovered %lu prime numbers in %lu.%lu seconds.\n",
+                 prime_index,
+                 ts_diff.tv_sec, (ts_diff.tv_nsec * 1000000000) / 10);
   (void) fflush(fp);
 
   if (filename)
     (void) fclose(fp);
 }
 
-static bool is_prime(uint64_t x)
+extern bool is_prime(uint64_t x)
 {
   if (x == 2)
     return true;
@@ -137,24 +113,6 @@ static bool is_prime(uint64_t x)
   }
 
   return true;
-}
-
-static bool mersenne(uint64_t x) {
-    if (x < 3)
-    return false;
-
-  for (unsigned i = 0; i < 65; ++i) {
-    if (PT[i] > x) {
-      uint64_t d = PT[i] - x;
-      if (d == 1ULL)
-        return true;
-
-      if (d > 1ULL)
-        break;
-    }
-  }
-
-  return false;
 }
 
 static int check_bits(unsigned bits)
@@ -178,32 +136,35 @@ static int allocate_storage(void)
 
   // Crude approximation. This is guaranteed to over-allocate.
   if (span < 10000UL)
-    nprimes = span;
+    nprimes = 5000UL;
   else if (span < 1000000UL)
-    nprimes = span / 5UL;
+    nprimes = 100000UL;
+  else if (span < 1000000000UL)
+    nprimes = 1000000UL;
+  else if (span < 1000000000000UL)
+    nprimes = 10000000UL;
   else
-    nprimes = span / 10UL;
+    nprimes = 100000000UL;
 
   errno = 0;
 
-  if ((prime_storage = malloc(nprimes * sizeof(uint64_t))) == NULL) {
+  size_t sz = nprimes * sizeof(uint64_t);
+  if ((prime_storage = malloc(sz)) == NULL) {
     (void) fprintf(stderr,
                    "Unable to allocate storage for sequence of primes: %s\n",
                    strerror(errno));
     return -1;
   }
 
-  (void) memset(prime_storage, 0, nprimes * sizeof(uint64_t));
+  (void) memset(prime_storage, 0, sz);
   return 0;
 }
 
-static void add_prime(uint64_t x)
+extern void add_prime(uint64_t x)
 {
-#if defined(_OPENMP)
-#pragma omp critical
-#endif
-
+  pthread_mutex_lock(&mutex);
   prime_storage[prime_index++] = x;
+  pthread_mutex_unlock(&mutex);
 }
 
 static int print_primes(const char* filename)
@@ -224,15 +185,8 @@ static int print_primes(const char* filename)
     (void) fprintf(fp, "List of prime numbers in the range %lu - %lu:\n\n",
                    range_start, range_end);
 
-  for (uint64_t i = 0; i < prime_index; ++i) {
-    (void) fprintf(fp, "%lu", prime_storage[i]);
-    if (print_mersenne) {
-      if (mersenne(prime_storage[i]))
-        (void) fprintf(fp, " [*M]");
-    }
-
-    (void) fprintf(fp, "\n");
-  }
+  for (uint64_t i = 0; i < prime_index; ++i)
+    (void) fprintf(fp, "%lu\n", prime_storage[i]);
 
   (void) fflush(fp);
 
@@ -258,17 +212,18 @@ static void find_primes(void)
   if ((effective_range_start % 2) == 0)
     effective_range_start += 1;
 
+  uint64_t k = effective_range_start;
+
   timestamp(&ts_begin);
 
-  uint64_t i;
-
 #if defined(_OPENMP)
-#pragma omp parallel for private (i)
+#pragma omp parallel for private (k)
 #endif
 
-  for (i = effective_range_start; i <= range_end; i += 2) {
-    if (is_prime(i))
-      add_prime(i);
+  for (k = effective_range_start; k <= range_end; k += 2) {
+    if (is_prime(k)) {
+      add_prime(k);
+    }
   }
 
   timestamp(&ts_end);
@@ -294,10 +249,10 @@ int main(int argc, char* argv[])
 {
   int opt;
   unsigned bits = 64;
-  bool ph = (argc == 1);
+  bool ph = false;
   const char* filename = NULL;
 
-  while ((opt = getopt(argc, argv, "hMpts:e:b:f:")) != -1) {
+  while ((opt = getopt(argc, argv, "hpts:e:b:f:")) != -1) {
     switch (opt) {
     case 'h':
       ph = true;
@@ -316,9 +271,6 @@ int main(int argc, char* argv[])
       break;
     case 'p':
       print_header = true;
-      break;
-    case 'M':
-      print_mersenne = true;
       break;
     case 't':
       print_timestamp = true;
@@ -346,17 +298,12 @@ int main(int argc, char* argv[])
   if (allocate_storage() != 0)
     return -1;
 
-#if defined(_OPENMP)
-  const char* nt;
-  if (nt = getenv("OMP_NUM_THREADS")) {
-    omp_set_num_threads((int) strtoul(nt, NULL, 10));
-  }
-#endif
-
   find_primes();
 
 #if defined(_OPENMP)
-  qsort(prime_storage, prime_index - 1, sizeof(uint64_t), qsort_compare);
+  if (prime_index >= 1UL) {
+    qsort(prime_storage, prime_index - 1, sizeof(uint64_t), qsort_compare);
+  }
 #endif
 
   if (print_primes(filename) != 0)
